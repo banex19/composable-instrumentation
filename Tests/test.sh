c180c7b57f5a822e40a7546f064bae4fbd229dd2
Daniele <test@test.com>
2018-03-19 20:04:28 +0000

    Added example output and fixed script

--- Tests/test.sh
+++ Tests/test.sh
@@ -3,18 +3,18 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo -e "${RED}Only tool instrumement.c\n${NC}"
+echo  "${RED}Only tool instrumement.c\n${NC}"
 clang++ -Wno-deprecated -Xclang -load  -Xclang ./../build/InstrumentPass/libInstrumentPass.so -mllvm "-instrument-tools" -mllvm "instrument.c" test.c instrument.c 
 ./a.out
 
-echo -e "\n${RED}Only tool time.cpp\n${NC}"
+echo  "\n${RED}Only tool time.cpp\n${NC}"
 clang++ -Wno-deprecated -Xclang -load  -Xclang ./../build/InstrumentPass/libInstrumentPass.so -mllvm "-instrument-tools" -mllvm "time.cpp" test.c time.cpp
 ./a.out
 
-echo -e "\n${RED}Both instrument.c and time.cpp - multiplexed for target program and no tool composing\n${NC}"
+echo  "\n${RED}Both instrument.c and time.cpp - multiplexed for target program and no tool composing\n${NC}"
 clang++ -Wno-deprecated -Xclang -load  -Xclang ./../build/InstrumentPass/libInstrumentPass.so -mllvm "-instrument-tools" -mllvm "instrument.c" -mllvm "-instrument-tools" -mllvm "time.cpp" -mllvm "-target-instrumentation" -mllvm "Multiplex" test.c instrument.c time.cpp
 ./a.out
 
-echo -e "\n${RED}Both instrument.c and time.cpp - only the first tool used for target program and chained composition for tools\n${NC}"
+echo  "\n${RED}Both instrument.c and time.cpp - only the first tool used for target program and chained composition for tools\n${NC}"
 clang++ -Wno-deprecated -Xclang -load  -Xclang ./../build/InstrumentPass/libInstrumentPass.so -mllvm "-instrument-tools" -mllvm "instrument.c" -mllvm "-instrument-tools" -mllvm "time.cpp" -mllvm "-target-instrumentation" -mllvm "FirstToolOnly"  -mllvm "-compose-function" -mllvm "Chained"  test.c instrument.c time.cpp
 ./a.out
